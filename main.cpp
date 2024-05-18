9fe593445d3d44a26e2c0fe0d6a56b9b23d81ae0
Muhammad Mehdi Ali <64282992+a-knaw-knee-mus@users.noreply.github.com>
2024-05-18 11:54:53 -0400

    move drawNodes to separate function

--- main.cpp
+++ main.cpp
@@ -11,6 +11,41 @@
 using namespace sf;
 using namespace std;
 
+void drawNodes(vector<shared_ptr<Node>> nodes, RenderWindow& window) {
+    for (auto& n : nodes) {
+        switch (n->state) {
+            case Clear:
+                n->node.setFillColor(Color::White);
+            break;
+            case Start:
+                n->node.setFillColor(Color(255, 0, 0));
+            break;
+            case End:
+                n->node.setFillColor(Color(255, 128, 0));
+            break;
+            case Visited:
+                n->node.setFillColor(Color(175, 238, 238));
+            break;
+            case Path:
+                n->node.setFillColor(Color(204, 153, 255));
+            break;
+            case InQueue:
+                n->node.setFillColor(Color(152, 251, 152));
+            break;
+            case CurrentNode:
+                n->node.setFillColor(Color(178, 102, 255));
+            break;
+            case VisitedNoPath:
+                n->node.setFillColor(Color(192, 192, 192));
+            break;
+            default:
+                n->node.setFillColor(Color::White);
+            break;
+        }
+        window.draw(n->node);
+    }
+}
+
 Color getEdgeColor(int weight) {
     switch(weight) {
         case 1:
@@ -28,6 +63,7 @@ Color getEdgeColor(int weight) {
     }
 }
 
+// draw arrowheads to show edge direction
 void drawArrowheads(unordered_map<shared_ptr<Node>, vector<pair<shared_ptr<Node>, int>>, NodePtrHash, NodePtrEqual>& edgeData, int nodeRadius, RenderWindow& window) {
     vector<ConvexShape> arrowheads{};
 
@@ -404,28 +440,9 @@ int main() {
                 window.draw(edgeShape);
             }
         }
-
-        // draw arrowheads to show edge direction
         drawArrowheads(edgeData, nodeRadius, window);
+        drawNodes(nodes, window);
 
-        // draw nodes
-        for (auto& n : nodes) {
-            switch (n->state) {
-                case Clear:
-                    n->node.setFillColor(Color::White);
-                    break;
-                case Start:
-                    n->node.setFillColor(Color(255, 0, 0));
-                    break;
-                case End:
-                    n->node.setFillColor(Color(255, 128, 0));
-                    break;
-                default:
-                    n->node.setFillColor(Color::White);
-                    break;
-            }
-            window.draw(n->node);
-        }
         window.display();
     }
 
